65d13345867a33cd136e8b2d690b86ccedd1ea2b
Ronan Rosales <ronanrosales123@gmail.com>
2023-11-28 17:47:16 +0800

    [test]

---
+++ lib/pages/components/SquareTile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  final String ImagePath;
+  const SquareTile({super.key, required this.ImagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset(ImagePath),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/components/SquareTile.dart';
+import 'components/loginbutton.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  void signIn() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 214, 211, 211),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 70), //spacer
+                Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+
+                SizedBox(height: 60), //spacer
+
+                Padding(
+                  //UserName
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      fillColor: Colors.grey,
+                      filled: true,
+                      hintText: 'Enter Username',
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10), //spacer
+
+                Padding(
+                  //Password
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      fillColor: Colors.grey,
+                      filled: true,
+                      hintText: 'Password',
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20), //spacer
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Forgot Password?'),
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 20), //spacer
+
+                SignInButton(
+                  onTap: signIn,
+                ),
+
+                SizedBox(height: 20), //spacer
+
+                SquareTile(ImagePath: 'lib/images/apple.png'),
+              ],
+            ),
+          ),
+        ));
+  }
+}
